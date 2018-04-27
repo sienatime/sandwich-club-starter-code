ad742ec372efaf8f6e6f03fc11a35832e6ac42fd
Siena Aguayo <siena@indiegogo.com>
2018-04-26 19:31:13 -0700

    basic stuff is working

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,15 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+import java.io.IOException;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -36,14 +39,23 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+        Sandwich sandwich = null;
+
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (IOException exception) {
+            closeOnError();
+            return;
+        }
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +68,15 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+      TextView alsoKnownAs = findViewById(R.id.also_known_tv);
+      TextView placeOfOrigin = findViewById(R.id.origin_tv);
+      TextView description = findViewById(R.id.description_tv);
+      TextView ingredients = findViewById(R.id.ingredients_tv);
 
+      alsoKnownAs.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+      placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+      description.setText(sandwich.getDescription());
+      ingredients.setText(TextUtils.join(", ", sandwich.getIngredients()));
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,72 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.JsonReader;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+  public static Sandwich parseSandwichJson(String json) throws IOException {
+    JsonReader reader = new JsonReader(new StringReader(json));
+    try {
+      return parseSandwich(reader);
+    } finally {
+      reader.close();
     }
+  }
+
+  private static List<String> parseStringList(JsonReader reader) throws IOException {
+    ArrayList<String> values = new ArrayList<>();
+    reader.beginArray();
+
+    while (reader.hasNext()) {
+      values.add(reader.nextString());
+    }
+
+    reader.endArray();
+    return values;
+  }
+
+  private static Sandwich parseSandwich(JsonReader reader) throws IOException {
+    Sandwich sandwich = new Sandwich();
+
+    //        reader.peek() != JsonToken.NULL
+
+    reader.beginObject();
+    while (reader.hasNext()) {
+      String key = reader.nextName();
+      if (key.equals("name")) {
+        // parse name object, which has mainName and array of strings alsoKnownAs
+        reader.beginObject();
+        while (reader.hasNext()) {
+          String nameKey = reader.nextName();
+          if (nameKey.equals("mainName")) {
+            sandwich.setMainName(reader.nextString());
+          } else if (nameKey.equals("alsoKnownAs")) {
+            List<String> alsoKnownAs = parseStringList(reader);
+            sandwich.setAlsoKnownAs(alsoKnownAs);
+          }
+        }
+        reader.endObject();
+      } else if (key.equals("placeOfOrigin")) {
+        sandwich.setPlaceOfOrigin(reader.nextString());
+      } else if (key.equals("description")) {
+        sandwich.setDescription(reader.nextString());
+      } else if (key.equals("image")) {
+        sandwich.setImage(reader.nextString());
+      } else if (key.equals("ingredients")) {
+        List<String> ingredients = parseStringList(reader);
+        sandwich.setIngredients(ingredients);
+      } else {
+        reader.skipValue();
+      }
+    }
+    reader.endObject();
+    return sandwich;
+  }
 }
