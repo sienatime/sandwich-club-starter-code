b928302cc6266d0a03dd3cd9df9f19e23e561e9b
Siena Aguayo <siena@indiegogo.com>
2018-04-26 20:27:34 -0700

    just a little json utils refactoring

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Attributes;
 
 public class JsonUtils {
 
@@ -32,41 +33,77 @@ public class JsonUtils {
     return values;
   }
 
-  private static Sandwich parseSandwich(JsonReader reader) throws IOException {
-    Sandwich sandwich = new Sandwich();
-
-    //        reader.peek() != JsonToken.NULL
+  private static NameJson parseNameJson(JsonReader reader) throws IOException {
+    NameJson nameJson = new NameJson();
 
     reader.beginObject();
     while (reader.hasNext()) {
-      String key = reader.nextName();
-      if (key.equals("name")) {
-        // parse name object, which has mainName and array of strings alsoKnownAs
-        reader.beginObject();
-        while (reader.hasNext()) {
-          String nameKey = reader.nextName();
-          if (nameKey.equals("mainName")) {
-            sandwich.setMainName(reader.nextString());
-          } else if (nameKey.equals("alsoKnownAs")) {
-            List<String> alsoKnownAs = parseStringList(reader);
-            sandwich.setAlsoKnownAs(alsoKnownAs);
-          }
-        }
-        reader.endObject();
-      } else if (key.equals("placeOfOrigin")) {
-        sandwich.setPlaceOfOrigin(reader.nextString());
-      } else if (key.equals("description")) {
-        sandwich.setDescription(reader.nextString());
-      } else if (key.equals("image")) {
-        sandwich.setImage(reader.nextString());
-      } else if (key.equals("ingredients")) {
-        List<String> ingredients = parseStringList(reader);
-        sandwich.setIngredients(ingredients);
-      } else {
-        reader.skipValue();
+      switch (reader.nextName()) {
+        case "mainName":
+          nameJson.setMainName(reader.nextString());
+          break;
+        case "alsoKnownAs":
+          List<String> alsoKnownAs = parseStringList(reader);
+          nameJson.setAlsoKnownAs(alsoKnownAs);
+          break;
+        default:
+          reader.skipValue();
+      }
+    }
+    reader.endObject();
+    return nameJson;
+  }
+
+  private static Sandwich parseSandwich(JsonReader reader) throws IOException {
+    Sandwich sandwich = new Sandwich();
+
+    reader.beginObject();
+    while (reader.hasNext()) {
+      switch (reader.nextName()) {
+        case "name":
+          NameJson nameJson = parseNameJson(reader);
+          sandwich.setMainName(nameJson.getMainName());
+          sandwich.setAlsoKnownAs(nameJson.getAlsoKnownAs());
+          break;
+        case "placeOfOrigin":
+          sandwich.setPlaceOfOrigin(reader.nextString());
+          break;
+        case "description":
+          sandwich.setDescription(reader.nextString());
+          break;
+        case "image":
+          sandwich.setImage(reader.nextString());
+          break;
+        case "ingredients":
+          List<String> ingredients = parseStringList(reader);
+          sandwich.setIngredients(ingredients);
+          break;
+        default:
+          reader.skipValue();
       }
     }
     reader.endObject();
     return sandwich;
+  }
+
+  private static class NameJson {
+    private String mainName;
+    private List<String> alsoKnownAs;
+
+    String getMainName() {
+      return mainName;
+    }
+
+    void setMainName(String mainName) {
+      this.mainName = mainName;
+    }
+
+    List<String> getAlsoKnownAs() {
+      return alsoKnownAs;
+    }
+
+    void setAlsoKnownAs(List<String> alsoKnownAs) {
+      this.alsoKnownAs = alsoKnownAs;
+    }
   }
 }
